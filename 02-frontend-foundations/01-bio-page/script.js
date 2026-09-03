204870d2b427950a9ca10faa8450d68395625d3a
Esther Royalty Akinfolahan <equeeny17@gmail.com>
2026-09-03 04:39:03 +0100

    Project list with user input and validation

--- 02-frontend-foundations/01-bio-page/script.js
+++ 02-frontend-foundations/01-bio-page/script.js
@@ -3,6 +3,8 @@ let count = 0;
 const counterDisplay = document.querySelector("#counter");
 const button = document.querySelector("#counter-btn");
 const projects = ["Calculator", "Budget Tracker", "AI Study Buddy", "Bio Page"];
+const input = document.querySelector("#new-project-input");
+const addButton = document.querySelector("#add-project-btn");
 
 const projectList = document.querySelector("#project-list");
 
@@ -10,6 +12,20 @@ projects.forEach(function (project) {
     const li = document.createElement("li");
     li.textContent = project;
     projectList.appendChild(li);
+});
+
+addButton.addEventListener("click", function () {
+    const newProject = input.value;
+
+    if (newProject === "") {
+        alert("Please type something first!");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = newProject;
+    projectList.appendChild(li);
+    input.value = "";
 });
 
 button.addEventListener("click", function () {
