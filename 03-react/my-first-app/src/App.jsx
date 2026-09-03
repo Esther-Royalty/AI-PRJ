4b006034e80c6174943252205b4c6bf7a7b626c5
Esther Royalty Akinfolahan <equeeny17@gmail.com>
2026-09-03 07:08:31 +0100

    React app with useState click counter

---
+++ 03-react/my-first-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>Count is {count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
+export default App
